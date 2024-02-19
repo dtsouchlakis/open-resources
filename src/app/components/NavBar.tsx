90980455b475b7709b6dcbebc687c10a454f65d8
christ8920 <christ8920@gmail.com>
2024-02-19 19:47:30 +0200

    Removed Sign out button, Added onClick to img, added profile options

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { sign } from "crypto";
 import { signOut } from "next-auth/react";
+import { useState } from "react";
 
 export default function NavBar() {
+
+  const [open, setOpen] = useState(false);
+  const [blur, setBlur] = useState(false);
+  function openDialoge() {
+    setOpen(!open);
+    setBlur(!blur);
+  }
+
   return (
     <div>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-1.5 rounded dark:bg-gray-800 flex flex-row justify-between items-center ">
-        <div className="container mx-auto">
-          <a href="https://flowbite.com/" className="flex items-center">
+        <div className="">
+          <a className="flex items-center">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="mr-3 h-6 sm:h-9"
@@ -19,26 +28,24 @@ export default function NavBar() {
           </a>
         </div>
         <div className="flex flex-row items-center justify-between gap-4">
-          <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
-            Button
-          </button>
-          <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
-            Button
-          </button>
-          <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
-            Button
-          </button>
-          <div className="rounded-full bg-gray-200 h-full w-10 overflow-hidden">
+          <div className="rounded-full bg-gray-200 h-full w-10 overflow-hidden cursor-pointer" 
+          onClick={
+            () => openDialoge()}>
             <img
               src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
               alt=""
+              className= { blur ? "transition duration-300 blur-[1.5px]" : "transition duration-300 blur-none" }
             />
-            <button
+            { open && <div className="absolute transform -translate-x-3/4 translate-y-1/4 border border-gray-700 rounded dark:bg-gray-800 p-2 shadow-md w-40">
+              <div className="transform duration-300 hover:bg-gray-700 rounded p-1">Profile</div>
+              <div className="transform duration-300 hover:bg-gray-700 rounded p-1" onClick={ () => signOut() }>Log out</div>
+            </div> }
+            {/* <button
               onClick={() => signOut()}
               className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
             >
               logout
-            </button>
+            </button> */}
           </div>
         </div>
       </nav>
