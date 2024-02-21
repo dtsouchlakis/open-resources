84cf603109de0fd323cadbfb92d1307f39cc6194
Dionysists <d.tsouchlakis@auroraer.com>
2024-02-21 20:37:27 +0900

    removed comments

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,7 +4,6 @@ import { signOut } from "next-auth/react";
 import { useState } from "react";
 
 export default function NavBar() {
-
   const [open, setOpen] = useState(false);
   function openDialoge() {
     setOpen(!open);
@@ -26,24 +25,28 @@ export default function NavBar() {
           </a>
         </div>
         <div className="flex flex-row items-center justify-between gap-4">
-          <div className="rounded-full bg-gray-200 h-full w-10 overflow-hidden cursor-pointer" 
-          onClick={
-            () => openDialoge()}>
+          <div
+            className="rounded-full bg-gray-200 h-full w-10 overflow-hidden cursor-pointer"
+            onClick={() => openDialoge()}
+          >
             <img
               src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
               alt=""
               className="transition duration-300 hover:blur-[1.5px]"
             />
-            { open && <div className="absolute transform -translate-x-3/4 translate-y-1/4 border border-gray-700 rounded dark:bg-gray-800 p-2 shadow-md w-40">
-              <div className="transform duration-300 hover:bg-gray-700 rounded p-1">Profile</div>
-              <div className="transform duration-300 hover:bg-gray-700 rounded p-1" onClick={ () => signOut() }>Log out</div>
-            </div> }
-            {/* <button
-              onClick={() => signOut()}
-              className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
-            >
-              logout
-            </button> */}
+            {open && (
+              <div className="absolute transform -translate-x-3/4 translate-y-1/4 border border-gray-700 rounded dark:bg-gray-800 p-2 shadow-md w-40">
+                <div className="transform duration-300 hover:bg-gray-700 rounded p-1">
+                  Profile
+                </div>
+                <div
+                  className="transform duration-300 hover:bg-gray-700 rounded p-1"
+                  onClick={() => signOut()}
+                >
+                  Log out
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </nav>
